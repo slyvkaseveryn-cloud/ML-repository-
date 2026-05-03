be03c02fed0687a5ca0caf015921d688f64b7f7d
Severyn Slyvka <slyvkaseveryn@gmail.com>
2026-05-03 22:15:32 +0300

    Додано початок проходження другого курсу Machine learning explainability, а саме реалізацію задач з частини Permutation importance

---
+++ Machine Learning Explainability/permutation_improtance.py
@@ -0,0 +1,36 @@
+import eli5
+from eli5.sklearn import PermutationImportance
+
+# Make a small change to the code below to use in this problem.
+perm = PermutationImportance(first_model, random_state=1).fit(val_X, val_y)
+
+q_2.check()
+
+# uncomment the following line to visualize your results
+# eli5.show_weights(perm, feature_names = val_X.columns.tolist())
+
+
+
+# create new features
+data['abs_lon_change'] = abs(data.dropoff_longitude - data.pickup_longitude)
+data['abs_lat_change'] = abs(data.dropoff_latitude - data.pickup_latitude)
+
+features_2  = ['pickup_longitude',
+               'pickup_latitude',
+               'dropoff_longitude',
+               'dropoff_latitude',
+               'abs_lat_change',
+               'abs_lon_change']
+
+X = data[features_2]
+new_train_X, new_val_X, new_train_y, new_val_y = train_test_split(X, y, random_state=1)
+second_model = RandomForestRegressor(n_estimators=30, random_state=1).fit(new_train_X, new_train_y)
+
+# Create a PermutationImportance object on second_model and fit it to new_val_X and new_val_y
+perm2 = PermutationImportance(second_model, random_state=1).fit(new_val_X, new_val_y)
+
+# show the weights for the permutation importance you just calculated
+eli5.show_weights(perm2, feature_names = new_val_X.columns.tolist())
+
+# Check your answer
+q_4.check()
